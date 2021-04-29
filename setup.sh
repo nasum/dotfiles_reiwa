c58e3f7db85fcc946a9d39902009c64f38e3885e
nasum <tomato.wonder.life@gmail.com>
2021-04-29 16:47:22 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'setup dotfiles'
 cp ./.zshrc ~/.zshrc
-cp ./.zshrc ~/.zshrc_`uname`
+cp ./.zshrc_`uname` ~/.zshrc_`uname`
 cp ./.gitconfig ~/.gitconfig
 cp ./.tmux.conf ~/.tmux.conf
 cp ./.spacemacs ~/.spacemacs
